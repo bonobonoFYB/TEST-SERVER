91974938cf67d31ecd67a67c1a86587d83ad4be2
Hyun Woo <hwsa10041@gmail.com>
2022-08-02 23:43:56 +0900

    로그인 컨트롤러 구현

--- src/main/java/com/example/API_test/controller/MemberController.java
+++ src/main/java/com/example/API_test/controller/MemberController.java
@@ -1,6 +1,8 @@
 package com.example.API_test.controller;
 
+import com.example.API_test.dto.MemberLoginDto;
 import com.example.API_test.dto.MemberRegisterDto;
+import com.example.API_test.model.TrueLogin;
 import com.example.API_test.service.MemberService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -15,10 +17,20 @@ public class MemberController {
 
     private final MemberService memberService;
 
+
     @PostMapping("/register")
     public MemberRegisterDto.Response registerMember(
             @Valid @RequestBody final MemberRegisterDto.Request request
     ) {
         return memberService.registerMember(request);
+    }
+
+    @PostMapping("/login")
+    public Object loginMember(
+            @Valid @RequestBody final MemberLoginDto.Request request
+    ) {
+        memberService.loginMember(request);
+        TrueLogin trueLogin = new TrueLogin();
+        return trueLogin;
     }
 }
