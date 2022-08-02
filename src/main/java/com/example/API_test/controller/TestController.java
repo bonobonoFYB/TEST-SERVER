76ae471626b0785d442bdb09ce14afc3ba4fb66a
Hyun Woo <hwsa10041@gmail.com>
2022-08-02 21:45:12 +0900

    Dto 구현
    -

--- src/main/java/com/example/API_test/controller/TestController.java
+++
@@ -1,7 +0,0 @@
-package com.example.API_test.controller;
-
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-public class TestController {
-}
---
+++ src/main/java/com/example/API_test/dto/MemberRegisterDto.java
@@ -0,0 +1,42 @@
+package com.example.API_test.dto;
+
+
+import com.example.API_test.entity.Member;
+import com.sun.istack.NotNull;
+import lombok.*;
+
+import javax.persistence.Entity;
+
+
+public class MemberRegisterDto {
+    @Getter
+    @Setter
+    @AllArgsConstructor
+    @NoArgsConstructor
+    @Builder
+    public static class Request{
+        @NotNull
+        private String name;
+        @NotNull
+        private String email;
+        @NotNull
+        private String pw;
+    }
+
+    @Getter
+    @Setter
+    @AllArgsConstructor
+    @NoArgsConstructor
+    @Builder
+    public static class Response{
+        private String name;
+        private String email;
+
+        public static Response entityResponse(@NotNull Member member){
+            return Response.builder()
+                    .name(member.getName())
+                    .email(member.getEmail())
+                    .build();
+        }
+    }
+}
