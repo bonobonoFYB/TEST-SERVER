653ab66ae61196dd1b237b30922193eb62de0d03
Hyun Woo <hwsa10041@gmail.com>
2022-08-02 23:44:23 +0900

    로그인 Dto 구현
    -

---
+++ src/main/java/com/example/API_test/dto/MemberLoginDto.java
@@ -0,0 +1,40 @@
+package com.example.API_test.dto;
+
+import com.example.API_test.entity.Member;
+import com.sun.istack.NotNull;
+import lombok.*;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MemberLoginDto {
+
+    @Getter
+    @Setter
+    @AllArgsConstructor
+    @NoArgsConstructor
+    @Builder
+    public static class Request{
+        @NotNull
+        private String email;
+        @NotNull
+        private String pw;
+    }
+
+    @Getter
+    @Setter
+    @AllArgsConstructor
+    @NoArgsConstructor
+    @Builder
+    public static class Response{
+        private String pw;
+        private String email;
+
+        public static MemberRegisterDto.Response entityResponse(@NotNull Member member){
+            return MemberRegisterDto.Response.builder()
+                    .name(member.getPw())
+                    .email(member.getEmail())
+                    .build();
+        }
+    }
+}
