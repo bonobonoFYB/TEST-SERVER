4ed2a9a481251e275d7651ce3389b25a2452753d
Hyun Woo <hwsa10041@gmail.com>
2022-08-02 23:44:56 +0900

    로그인 성공 모델 구현
    -

---
+++ src/main/java/com/example/API_test/model/TrueLogin.java
@@ -0,0 +1,11 @@
+package com.example.API_test.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class TrueLogin {
+    public Integer status = 200;
+    public String statusMessage = "로그인 성공";
+}
