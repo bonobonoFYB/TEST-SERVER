0e6f60dad8081407d6f027ec04b74f2b96f74821
Hyun Woo <hwsa10041@gmail.com>
2022-08-05 11:23:16 +0900

    DeatailService 구현

---
+++ src/main/java/com/example/API_test/service/CustomUserDetailsService.java
@@ -0,0 +1,46 @@
+package com.example.API_test.service;
+
+
+import com.example.API_test.entity.Member;
+import com.example.API_test.repository.memberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+// userDatailsSerivce를 오버라이드해야 AuthController 가 작동
+@Component("userDetailsService")
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+    private final memberRepository memberRepository;
+
+
+    @Override
+    @Transactional
+    public UserDetails loadUserByUsername(final String email) {
+        return memberRepository.findOneWithAuthoritiesByEmail(email)
+                .map(user -> createUser(email, user))
+                .orElseThrow(() -> new UsernameNotFoundException(email + " -> 데이터베이스에서 찾을 수 없습니다."));
+    }
+
+    private User createUser(String username, Member member) {
+//        if (!member.isActivated()) { -> 활성화 상태를 boolean 값으로 Entity에 받았을 경우
+//            throw new RuntimeException(username + " -> 활성화되어 있지 않습니다.");
+//        }
+        List<GrantedAuthority> grantedAuthorities = member.getAuthorities().stream()
+                .map(authority -> new SimpleGrantedAuthority(authority.getAuthorityName()))
+                .collect(Collectors.toList());
+        return new User(member.getEmail(),
+                member.getPw(),
+                grantedAuthorities
+        );
+    }
+}
