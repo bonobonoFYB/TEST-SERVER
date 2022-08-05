c47aae7de669edaad9731bb5c0d6dd741eb6145f
Hyun Woo <hwsa10041@gmail.com>
2022-08-05 11:33:53 +0900

    회원가입 서비스 구현

--- src/main/java/com/example/API_test/service/MemberService.java
+++ src/main/java/com/example/API_test/service/MemberService.java
@@ -1,28 +1,41 @@
 package com.example.API_test.service;
 
+import com.example.API_test.Exception.DuplicateMemberException;
 import com.example.API_test.dto.MemberLoginDto;
 import com.example.API_test.dto.MemberRegisterDto;
+import com.example.API_test.entity.Authority;
 import com.example.API_test.entity.Member;
-import com.example.API_test.repository.memberRepository;
+import com.example.API_test.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
-import org.springframework.web.server.ServerWebInputException;
 
 import javax.transaction.Transactional;
+import java.util.Collections;
 
 @Service
 @RequiredArgsConstructor
 public class MemberService {
 
-    private final memberRepository memberRepository;
+    private final MemberRepository memberRepository;
+
+    private final PasswordEncoder passwordEncoder;
 
     @Transactional
     public MemberRegisterDto.Response registerMember(MemberRegisterDto.Request request) {
+        if (memberRepository.findOneWithAuthoritiesByEmail(request.getEmail()).orElse(null) != null) {
+            throw new DuplicateMemberException("이미 가입되어 있는 유저입니다.");
+        }
+        Authority authority = Authority.builder()
+                .authorityName("ROLE_USER")
+                .build();
+
         return MemberRegisterDto.Response.entityResponse(
                 memberRepository.save(
                         Member.builder()
                                 .name(request.getName())
                                 .email(request.getEmail())
+                                .authorities(Collections.singleton(authority))
                                 .pw(request.getPw())
                                 .build()
                 )
